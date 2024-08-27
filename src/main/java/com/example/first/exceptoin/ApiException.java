6b64754ef68ccaac147361a847e6c8f4db9e3edd
Anastasiya <n.bogocharova@gmail.com>
2024-08-27 18:30:41 +0300

    added ApiException

---
+++ src/main/java/com/example/first/exceptoin/ApiException.java
@@ -0,0 +1,25 @@
+package com.example.first.exceptoin;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+@EqualsAndHashCode(callSuper = false)
+@Data
+public class ApiException extends RuntimeException {
+    private int statusCode;
+
+    public ApiException(String message, int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public String toString() {
+        return "{\n" +
+                "  \"statusCode\": " + statusCode + ",\n" +
+                "  \"message\": \"" + getMessage() + "\"\n" +
+                "}";
+    }
+}
+
